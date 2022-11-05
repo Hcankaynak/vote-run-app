da5f762e3ebc4716b1f234a24089751dd8cd1ba8
Hüseyincan <h.cankaynak@gmail.com>
2022-11-05 17:34:11 +0300

    react-bootsrap and sass added.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,27 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.css";
+import "./homepage.scss";
+
 interface HomePageProps {}
 
 const HomePage = () => {
   return (
-    <div>
-      <p>This is the homepage.</p>
+    <div className="home-page">
+      <Form>
+        <Form.Group className="email" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="pass" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(value) => console.log(value.target.value)}
+          />
+        </Form.Group>
+      </Form>
     </div>
   );
 };
