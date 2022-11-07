073026c21663437208786615073771d4a88fcdee
hcankaynak <h.cankaynak@gmail.com>
2022-11-07 21:44:39 +0300

    files formatted

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,25 +1,23 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-
-interface AboutPageProps {}
+import {useParams} from "react-router-dom";
 
 const AboutPage = () => {
-  const [message, setMessage] = React.useState("");
-  const { number } = useParams();
+    const [message, setMessage] = React.useState("");
+    const {number} = useParams();
 
-  React.useEffect(() => {
-    if (number) {
-      setMessage("There is your number: " + number);
-    } else {
-      setMessage("No number provided");
-    }
-  }, []);
-  return (
-    <div>
-      <p>This is About Page.</p>
-      <p>{message}</p>
-    </div>
-  );
+    React.useEffect(() => {
+        if (number) {
+            setMessage("There is your number: " + number);
+        } else {
+            setMessage("No number provided");
+        }
+    }, []);
+    return (
+        <div>
+            <p>This is About Page.</p>
+            <p>{message}</p>
+        </div>
+    );
 };
 
 export default AboutPage;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,27 +3,26 @@ import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.css";
 import "./homepage.scss";
 
-interface HomePageProps {}
 
 const HomePage = () => {
-  return (
-    <div className="home-page">
-      <Form>
-        <Form.Group className="email" controlId="formGroupEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group className="pass" controlId="formGroupPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            onChange={(value) => console.log(value.target.value)}
-          />
-        </Form.Group>
-      </Form>
-    </div>
-  );
+    return (
+        <div className="home-page">
+            <Form>
+                <Form.Group className="email" controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email"/>
+                </Form.Group>
+                <Form.Group className="pass" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        onChange={(value) => console.log(value.target.value)}
+                    />
+                </Form.Group>
+            </Form>
+        </div>
+    );
 };
 
 export default HomePage;
